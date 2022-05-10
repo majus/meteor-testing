348963dbaac7da16c581fc57f085bdba2d83e8ef
Denis Perov <denis.perov@gmail.com>
2022-05-10 16:25:31 +0300

    Stub collection indexing methods

--- collections.js
+++ collections.js
@@ -2,13 +2,20 @@
 import { LocalCollection } from 'meteor/minimongo';
 import { sinon } from '@majus/testing';
 
+class CustomLocalCollection extends LocalCollection {
+
+  _ensureIndex = sinon.stub();
+  createIndex = sinon.stub();
+
+}
+
 export const StubCollections = {
   sandbox: sinon.createSandbox(),
   collections: [],
   stub(...collections) {
     for (const collection of collections) {
       if (!this.collections.includes(collection)) {
-        const replacement = new LocalCollection(collection._name);
+        const replacement = new CustomLocalCollection(collection._name);
         this.sandbox.stub(collection, '_collection').value(replacement);
         this.sandbox.stub(collection, '_connection').value();
         this.collections.push(collection);
